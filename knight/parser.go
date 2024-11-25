219cbb4a31d877b30fe67ea09ffb19b20bc06182
SamW <mail@sampersand.me>
2024-11-25 15:05:19 -0800

    cleanup parser.go

--- knight/parser.go
+++ knight/parser.go
@@ -7,6 +7,12 @@ import (
 	"unicode"
 )
 
+// EndOfInput indicates that Parser.ParseNextValue was called when the input source was empty.
+//
+// This is a user error: They either provided a program which was exclusively whitespace/comments,
+// or didn't provide enough arguments to a function (eg `DUMP + 1`).
+var EndOfInput = errors.New("source was empty")
+
 // Parser is used to construct Values from source code.
 //
 // This parses Knight programs in terms of `rune`s (golang speak for utf-8 characters), not bytes.
@@ -79,11 +85,20 @@ func (p *Parser) TakeWhile(condition func(rune) bool) string {
 	return string(p.source[start:p.index])
 }
 
-// EndOfInput indicates that Parser.ParseNextValue was called when the input source was empty.
-//
-// This is a user error: They either provided a program which was exclusively whitespace/comments,
-// or didn't provide enough arguments to a function (eg `DUMP + 1`).
-var EndOfInput = errors.New("source was empty")
+/**
+ * Functions used within `TakeWhile`
+ **/
+func isntNewLine(r rune) bool { return r != '\n' }
+func isDigit(r rune) bool { return '0' <= r && r <= '9' }
+func isVariableStart(r rune) bool { return unicode.IsLower(r) || r == '_' }
+func isVariableCharacter(r rune) bool { return isVariableStart(r) || unicode.IsNumber(r) }
+func isWordFunctionCharacter(r rune) bool { return unicode.IsUpper(r) || r == '_' }
+func isWhitespace(r rune) bool {
+	// Note: The parenthesis are also included here because they may also safely be considered
+	// whitespace by implementations. (There's an optional extension where implementations can give
+	// syntax errors on unbalanced parenthesis if they want, but this implementation isn't doing that.)
+	return unicode.IsSpace(r) || r == '(' || r == ')'
+}
 
 // ParseNextValue returns the next Value in the source code. EndOfInput is returned if there's no
 // Values left. Syntax errors (such as missing an ending quote) are also returned.
@@ -95,38 +110,26 @@ func (p *Parser) ParseNextValue() (Value, error) {
 
 	// Determine what to do solely based upon the next character. (Knight's designed in such a way
 	// that you can always unambiguously parse based on just the next character in the input stream.)
-	//
-	// Because some of the cases require calling functions (e.g. `unicode.IsLower(c)` for variables),
-	// we can't do the more traditional `switch p.Peek() {` syntax. 
 	c := p.Peek()
 	switch {
-
-	// Whitespace and Parenthesis, delete it and go again.
-	//
-	// The parenthesis are also included here because they may also safely be considered whitespace
-	// by implementations. (There's an optional extension where implementations can give syntax
-	// errors on unbalanced parenthesis if they want, but this implementation isn't doing that.)
-	case unicode.IsSpace(c) || c == '(' || c == ')':
+	// Whitespace, delete it and go again.
+	case isWhitespace(c):
 		p.Advance()
 		return p.ParseNextValue()
 
-	// Comment. Delete it, and then parse again.
+	// Comment, delete it and go again.
 	case c == '#':
-		_ = p.TakeWhile(func (r rune) bool { return r != '\n' })
+		_ = p.TakeWhile(isntNewLine)
 		return p.ParseNextValue()
 
 	// Integers
-	case '0' <= c && c <= '9':
-		integerString := p.TakeWhile(func(r rune) bool { return '0' <= r && r <= '9' })
-		integer, _ := strconv.Atoi(integerString)
+	case isDigit(c):
+		integer, _ := strconv.Atoi(p.TakeWhile(isDigit))
 		return Integer(integer), nil
 
 	// Variables
-	case unicode.IsLower(c), c == '_':
-		variableName := p.TakeWhile(func(r rune) bool {
-			return r == '_' || unicode.IsLower(r) || unicode.IsDigit(r)
-		})
-		return NewVariable(variableName), nil
+	case isVariableStart(c):
+		return NewVariable(p.TakeWhile(isVariableCharacter)), nil
 
 	// Strings
 	case c == '\'' || c == '"':
@@ -145,17 +148,18 @@ func (p *Parser) ParseNextValue() (Value, error) {
 		p.Advance()
 		return String(contents), nil
 
-	// Everything else is a function (or a parse error)
+	// Everything else is a function, or invalid (which we check for below).
 	default:
-		// Try to take a word function; if that doesn't work, then just Advance a single rune.
-		if "" == p.TakeWhile(func (r rune) bool { return unicode.IsUpper(r) || r == '_' }) {
+		startIndex := p.index // used for error msgs
+
+		// Delete the function name out
+		if isWordFunctionCharacter(c) {
+			_ = p.TakeWhile(isWordFunctionCharacter)
+		} else {
 			p.Advance()
 		}
 
-		startIndex := p.index // for error msgs
-
-		// Get the function definition. If it doesn't exist, then the user's given us an invalid program,
-		// and we error out.
+		// Get the function definition; If it doesn't exist, then we've been given an invalid token.
 		function, ok := KnownFunctions[c]
 		if !ok {
 			return nil, fmt.Errorf("[line %d] unknown token start: %c", p.linenoAt(startIndex), c)
@@ -166,18 +170,17 @@ func (p *Parser) ParseNextValue() (Value, error) {
 
 		// Parse each argument and add them to the `arguments`.
 		for i := 0; i < function.arity; i++ {
-			var err error
-
-			// Try to parse the argument; if it's not successful, handle the error.
-			if arguments[i], err = p.ParseNextValue(); err != nil {
+			argument, err := p.ParseNextValue()
+			if err != nil {
 				// Special case: If the error was `EndOfInput`, provide a better error message.
 				if err == EndOfInput {
-					err = fmt.Errorf("[line %d] missing argument %d for function %q",
-						p.linenoAt(startIndex), i + 1, function.name)
+					return nil, fmt.Errorf("[line %d] missing argument %d for function %q",
+							p.linenoAt(startIndex), i + 1, function.name)
 				}
-
 				return nil, err
 			}
+
+			arguments[i] = argument
 		}
 
 		return NewAst(function, arguments), nil
