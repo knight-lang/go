47d2d8f91a3bef9aaf498496a6c331ef95fc71d4
SamW <mail@sampersand.me>
2024-11-26 16:45:05 -0800

    updated main.go

--- main.go
+++ main.go
@@ -8,19 +8,35 @@ import (
 	"github.com/knight-lang/go/knight"
 )
 
-func die(fmtstr string, rest ...interface{}) {
-	fmt.Fprintf(os.Stderr, fmtstr, rest...)
-	fmt.Fprint(os.Stderr, "\n")
-	os.Exit(1)
+// printAndExit 
+func printAndExit(isErr bool, fmtstr string, rest ...interface{}) {
+	var out *os.File
+
+	if isErr {
+		out = os.Stderr
+	} else {
+		out = os.Stdout
+	}
+
+
+	fmt.Fprintf(out, fmtstr, rest...)
+	fmt.Fprint(out, "\n")
+
+	if isErr {
+		os.Exit(1)
+	} else {
+		os.Exit(0)
+	}
 }
 
-func usage() {
-	die("usage: %s (-e 'expr' | -f file)", os.Args[0])
+// Prints the usage and exits. isErr should be 
+func usage(isErr bool) {
+	printAndExit(isErr, "usage: %s [-h] (-e 'expr' | -f file)", os.Args[0])
 }
 
 func main() {
 	if len(os.Args) != 3 {
-		usage()
+		usage(true)
 	}
 
 	var program string
@@ -31,15 +47,18 @@ func main() {
 	case "-f":
 		programBytes, err := ioutil.ReadFile(os.Args[2])
 		if err != nil {
-			die("couldn't read file contents: %s", err)
+			printAndExit(true, "[FATAL] Couldn't read file contents: %s", err)
 		}
 		program = string(programBytes)
 
+	case "-h":
+		usage(false)
+
 	default:
-		usage()
+		usage(true)
 	}
 
 	if _, err := knight.Play(program); err != nil {
-		die("%s", err)
+		printAndExit(true, "%s", err)
 	}
 }
